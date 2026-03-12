37ba4cad25a4ab609eef924552108b588aac5fd8
unknown <kairenz@eoeoeo.net>
2026-03-12 11:56:07 +0700

    simplify /design handler to ack only

--- bot.py
+++ bot.py
@@ -284,8 +284,6 @@ def get_image_url(event):
 @bolt_app.command("/design")
 def handle_design(ack, respond, command):
     ack()
-    text = command["text"]
-    respond(f"Bạn hỏi: {text}")
 
 
 # -------------------------
